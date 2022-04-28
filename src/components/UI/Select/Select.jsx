16272d953947d53285336e960b3c360116713147
Ajay Gangwar <gangwar.ajay@orangemantra.in>
2022-04-28 22:26:40 +0530

    Added education

--- src/components/UI/Select/Select.jsx
+++ src/components/UI/Select/Select.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import styles from './select.module.scss';
 
 const Select = ({ type }) => {
     let data = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
@@ -6,17 +7,17 @@ const Select = ({ type }) => {
         data = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2022', '2023'];
     }
 
-
-
     return (
-        <div><select>
-            {
-                data.map((month, i) => {
-                    return <option key={i} value={i + 1}>{month}</option>
-                })
+        <div className={styles.Inputholder}>
+            <select className={styles.Inputbox}>
+                {
+                    data.map((month, i) => {
+                        return <option key={i} value={i + 1}>{month}</option>
+                    })
 
-            }
-        </select></div>
+                }
+            </select>
+        </div>
     )
 }
 
--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import Progress from '../../components/Progress/Progress';
 import Input from '../../components/UI/Input/Input';
 import styles from './experience.module.scss';
-import { useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
-import PersonIcon from '@mui/icons-material/Person';
 import Button from '../../components/UI/Button/Button';
 import Select from '../../components/UI/Select/Select';
+import CancelIcon from '@mui/icons-material/Cancel';
+import AddIcon from '@mui/icons-material/Add';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 const Experience = () => {
 
+    const [show, setShow] = useState(false)
     const [formData, setFormData] = useState({
         firstName: {
             value: "",
@@ -21,18 +23,6 @@ const Experience = () => {
         console.log("its Clicked")
     }
 
-    const onDrop = useCallback(acceptedFiles => {
-
-        console.log(acceptedFiles[0].path)
-
-        let formData = new FormData();
-
-        formData.append("image", acceptedFiles[0]);
-
-    }, [])
-
-    const { getRootProps, isDragActive } = useDropzone({ onDrop });
-
     return (
         <div className={styles.Container}>
             <div className={styles.subContainer}>
@@ -40,33 +30,75 @@ const Experience = () => {
                     <p>Experience</p>
                     <Progress />
                 </div>
-                <div className={styles.box}>
 
-                    <div className={styles.contact}>
-                        <div>
-                            <Input placeholder="Job title" name="jobTitle" error={false} />
-                            <Input placeholder="Company or Project" name="companyProject" />
-                            <Input placeholder="Personal Website" name="firstName" />
-                        </div>
-                    </div>
-
-                    <div className={styles.contact}>
-                        <div className={styles.flex}>
-                            <Select type="month" />
-                            <Select type="year" />
-                        </div>
-                    </div>
-
-                    <div className={styles.contact}>
-                        <div className={styles.flex}>
-                            <Input placeholder="Accomplishments" name="accomplishments" />
-                        </div>
-                    </div>
-
+                <div className={styles.Addnew} onClick={() => setShow(!show)}>
+                    <AddIcon />
+                    <p>Add Experience</p>
                 </div>
-                <div className={styles.buttonHolder}>
-                    <Button click={handleClick} text="Submit" />
-                </div>
+
+                {
+                    show
+                        ? <div>
+                            <div className={styles.box}>
+                                <div className={styles.closeButton}>
+                                    <CancelIcon onClick={() => setShow(false)} />
+                                </div>
+
+                                <div className={styles.contact}>
+                                    <div>
+                                        <Input placeholder="Job title" name="jobTitle" error={false} />
+                                        <Input placeholder="Company or Project" name="companyProject" />
+                                        <Input placeholder="Personal Website" name="firstName" />
+                                    </div>
+                                </div>
+
+                                <div className={styles.contact}>
+                                    <div className={styles.flex}>
+                                        <Select type="month" />
+                                        <Select type="year" />
+                                        <div>-</div>
+                                        <Select type="month" />
+                                        <Select type="year" />
+                                    </div>
+                                </div>
+
+                                <div className={styles.checkholder}>
+                                    <div className={styles.check}>
+                                        <input type="checkbox" name="current" />
+                                        <label>Currently Working here</label>
+                                    </div>
+                                </div>
+
+                                <div className={styles.contact}>
+                                    <Input placeholder="Accomplishments" name="accomplishments" />
+
+                                </div>
+
+                            </div>
+                            <div className={styles.buttonHolder}>
+                                <Button click={handleClick} text="Submit" />
+                            </div>
+                        </div>
+                        : <div>
+                            <div className={styles.box}>
+                                <div className={styles.inner}>
+                                    <div className={styles.information}>
+                                        <h2>Frontend Developer</h2>
+                                        <p>Google</p>
+                                    </div>
+
+                                    <div className={styles.icons}>
+                                        <EditOutlinedIcon />
+                                        <DeleteOutlineOutlinedIcon />
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className={styles.buttonHolder}>
+                                <Button click={handleClick} text="Continue" />
+                            </div>
+                        </div>
+                }
             </div>
         </div>
     )
